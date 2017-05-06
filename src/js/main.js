02c26237af00e90c22ef51327b1017a27e1d6416
Quentin <quentinlemarchand0214@gmail.com>
2017-05-06 21:55:29 +0200

    start products view

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
-console.log('obj');
+var	slider      = document.querySelector('.images'),
+	  left_arrow  = document.querySelector('.left-arrow > img'),
+		right_arrow = document.querySelector('.right-arrow > img')
+
+left_arrow.addEventListener('click', function(e) {
+	console.log(slider.style.backgroundImage);
+	slider.style.backgroundImage = "url('../src/img/other-product-4.jpg')"
+})
+
+right_arrow.addEventListener('click', function(e) {
+	console.log('right');
+})
